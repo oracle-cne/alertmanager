ae6e6ef779d8a22b8650b68bb3ae47258033f65f
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-09-12 09:28:43 -0400

    converting to image build

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -1,24 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir -p bin
 name="alertmanager"
 version="0.28.1"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
-
-GIT_REVISION=$(git rev-parse --long HEAD)
-BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-ldflags="
-        -X main.version=v%{version}
-        -X github.com/prometheus/common/version.Version=%{version}
-        -X github.com/prometheus/common/version.Revision=${GIT_REVISION}
-        -X github.com/prometheus/common/version.Branch=HEAD
-        -X github.com/prometheus/common/version.BuildUser=${USER}@${HOST}
-        -X github.com/prometheus/common/version.BuildDate=${BUILD_DATE}"
-go build -trimpath=false -v -o bin/ \
-    -ldflags "${ldflags}" \
-    ${GOPATH_SRC}/cmd/alertmanager \
-    ${GOPATH_SRC}/cmd/amtool
 
 docker build --pull \
     --build-arg https_proxy=${https_proxy} \
