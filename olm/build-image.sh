175519f52a959525b2217decb15f523dcc415ca1
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-11-05 14:23:47 -0500

    fix syntax error in build-image.sh

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 name="alertmanager"
-version={{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
+version="{{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
 registry="container-registry.oracle.com/olcne"
 docker_tag=${registry}/${name}:v${version}
 
